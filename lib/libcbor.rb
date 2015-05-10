1b87e4006928b23e02cb150000575067d2f2beaf
PJK <me@pavelkalvoda.com>
2015-05-10 21:02:20 +0200

    Build cmocka as well

--- lib/libcbor.rb
+++ lib/libcbor.rb
@@ -14,17 +14,32 @@ require 'libcbor/streaming/callback_simplifier'
 require 'libcbor/streaming/buffered_decoder'
 require 'libcbor/streaming/encoder'
 
+# Top level namespace
 module CBOR
+	# Thrown when the decoded data is either invalid or not well-formed
 	class DecodingError < StandardError; end
 
-	def self.encode(obj)
-		obj.to_cbor
+	# Encodes the given object to CBOR representation
+	# Custom semantics are supported - the object has to to provide the +to_cbor+ method
+	# In case {CBOR.method_name} is not +to_cbor+, provide the custom method instead.
+	#
+	# @param [Fixnum, String, Float, Array, Map, Tag, #to_cbor] object the object to encode
+	# @return [String] the CBOR representation
+	def self.encode(object)
+		object.to_cbor
 	end
 
-	def method_name
+	# The name of the method that is used for encoding objects. Defaults to +:to_cbor+
+	#
+	# @return [Symbol] the method name
+	def self.method_name
 		@@method_name
 	end
 
+	# Load the extensions for the core classes
+	#
+	# @param [Symbol] name the name to use for the encoding method. If not provided, +to_cbor+ will be used
+	# @return nil
 	def self.load!(name = nil)
 		@@method_name = name
 		%w{Fixnum Float Array Hash String TrueClass FalseClass NilClass Tag}.each do |klass|
